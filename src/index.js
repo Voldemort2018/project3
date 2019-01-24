98963e4927a64834f5b2e17018b4349d1fcf7e45
Javier Vega <jvega@Javiers-MacBook-Pro.local>
2019-01-23 19:22:18 -0600

    newlayout images, modals, navbar

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as serviceWorker from './serviceWorker';
 
-let state = {};
-window.setState = (changes) => {
-    state = Object.assign({}, state, changes);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-    ReactDOM.render(<App {...state}/>, document.getElementById('root'));
-}
-
-/* eslint no-restricted-globals: 0*/
-
-let initialState = {
-    name: "amanda",
-    location: location.pathname.replace(/^\/?|\/$/g, "")
-};
-
-window.setState(initialState);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
