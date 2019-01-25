a4b36e556f2f5fe04da236ccb95b75e9e885ca1c
motoko104 <motoko104@yahoo.com>
2019-01-24 19:53:18 -0600

    trying to fix deloyment

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
-import ModalLogin  from './components/modal/Modal';
+import Navbar from './Components/navbar/Navbar';
+import Home from './Components/home/Home';
+import ModalLogin  from './Components/modal/Modal';
 
 class App extends Component {
   render() {
