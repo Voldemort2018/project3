1b43a421cce20b30bde017541fda30b4a3dcc7fa
Michael Wenhold <40506588+michaelwenhold@users.noreply.github.com>
2019-01-25 19:33:07 -0600

    index.js

--- models/event.js
+++ models/event.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const eventSchema = new Schema({
+const eventsSchema = new Schema({
   event_name: { type: String, required: true },
   event_date: { type: Date, default: Date.now, required: true },
   event_time: { type: String, required: true }
 });
 
-const Event = mongoose.model("Event", eventSchema);
+const Events = mongoose.model("Events", eventsSchema);
 
-module.exports = Event;
+module.exports = Events;
