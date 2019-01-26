c75b67b7be4c651876eb97abe5a207a4e6838657
Stephanie Gonzales <stephgonza@utexas.edu>
2019-01-26 12:42:27 -0600

    changes on UI that fixed local break, added incoming change from develop

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/navbar/Navbar';
 import Home from './Components/home/Home';
-<<<<<<< HEAD
-import Users  from './Components/users/Users';
-=======
 import Users  from './Components/users/User';
->>>>>>> f1439bf7b94c42443ad6755e589c78e43d2e31b5
 
 class App extends Component {
   render() {
