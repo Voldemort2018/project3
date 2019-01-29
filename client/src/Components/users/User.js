c2a18f4dfccda3400fff76097ca34dfcd726372f
motoko104 <motoko104@yahoo.com>
2019-01-28 20:07:04 -0600

    reverting back to the way things were when it was all working

--- client/src/Components/users/User.js
+++ client/src/Components/users/User.js
@@ -5,8 +5,6 @@ import MapContainer from '../map/Map';
 import Navbar from '../navbar/Navbar';
 import './users.css';
 
-console.log(props);
-
 export default class Users extends React.Component {
 	constructor (){
 		super();
