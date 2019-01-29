fae0c13412d89707ced8e337e6f72c7a1eb90967
Stephanie Gonzales <stephgonza@utexas.edu>
2019-01-29 13:31:55 -0600

    pushing to branch devs to pull from develop

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  user_name: { type: String, required: true },
+  user_name: { type: String, required: true, unique: true },
   user_email: { type: String, required: true },
   user_phone: { type: String, required: true }
 });
